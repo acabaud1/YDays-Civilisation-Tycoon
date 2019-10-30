8e962fa7e21dc230b1d7ef7ffb0ad271f0f35aeb
h3r0xfr <adrien.cabaud@numericable.fr>
2019-10-30 16:15:51 +0100

    adding ResourcesManager

---
+++ Assets/Scripts/ResourceManagerTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManagerTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject resourceManagerGameObject = GameObject.Find("ResourceManager");
+        ResourceManager resourceManager = resourceManagerGameObject.GetComponent<ResourceManager>();
+        var iron = resourceManager.Get(typeof(Iron));
+        Debug.Log(iron.Name);
+        Debug.Log(iron.Quantity);
+        resourceManager.Add(typeof(Iron), 10);
+        Debug.Log(iron.Quantity);
+        iron.Quantity = iron.Quantity - 5;
+        Debug.Log(iron.Quantity);
+  }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
