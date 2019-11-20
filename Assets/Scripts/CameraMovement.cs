8695cf2d68cc8c024f348b67c5d9350eaf506ee0
Cedro23 <cedric1lesueur@gmail.com>
2019-11-20 11:25:13 +0100

    Script caméra : création script déplacement et zoom de caméra | zoom fini | déplacement en train d'être amélioré

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 10.0f;
+
+    private float horizontalInput;
+    private float verticalInput;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || verticalInput != 0)
+        {
+            transform.position += moveSpeed * new Vector3(horizontalInput, 0, verticalInput) * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    //public variables paremeters for Unity inpector inputs
+    public float initialFOV;
+    public float zoomInFOV;
+    public float speed;
+
+    //private variables for script mod
+    private float currentFOV;
+    private float scrollWheelInput;
+
+    // Use this for initialization
+    void Start()
+    {
+        //set initial FOV at start
+        Camera.main.fieldOfView = initialFOV;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scrollWheelInput = Input.GetAxis("Mouse ScrollWheel");
+        //store current field of view value in variable
+        currentFOV = Camera.main.fieldOfView;
+        if (scrollWheelInput != 0)
+        {
+            if (currentFOV <= initialFOV && currentFOV >= zoomInFOV)
+            {
+                Camera.main.fieldOfView += (speed * -scrollWheelInput) * Time.deltaTime;
+            }
+            else if (currentFOV < zoomInFOV && scrollWheelInput < 0)
+            {
+                Camera.main.fieldOfView += (speed * -scrollWheelInput) * Time.deltaTime;
+            }
+            else if (currentFOV > initialFOV && scrollWheelInput > 0)
+            {
+                Camera.main.fieldOfView += (speed * -scrollWheelInput) * Time.deltaTime;
+            }
+        }
+
+    }
+}
