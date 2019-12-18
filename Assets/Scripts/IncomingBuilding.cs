8dda0a838d978f2cc05bd85996be1030267c7c4a
Louis Charavner <distil47@gmail.com>
2019-12-18 13:44:44 +0100

    income buidling WIP

---
+++ Assets/Scripts/IncomingBuilding.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class IncomingBuilding : MonoBehaviour
+{
+
+    public string ResourceName;
+    private ParticleSystemAnimationType.
+
+    void Start()
+    {
+        try
+        {
+            
+        } 
+        catch (System.NullReferenceException e)
+        {
+            Debug.LogError($"Le type {ResourceName} n'existe pas.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
